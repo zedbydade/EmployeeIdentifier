3217a5116919113ca19cd17d48e6b0c1bf0bf7b6
Bruno <brunotj2015@hotmail.com>
2019-08-04 01:50:52 -0300

    :construction: creates Auth middleware

---
+++ src/http/middlewares/Auth.ts
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import { NextFunction, Response } from "express";
+
+export const TokenValidator = async (
+  request: any,
+  response: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    // @ts-ignore
+    const [_, token] = request.headers.authorization.split(" ");
+    request.token_payload = await jwt.verify(token, process.env
+      .JWT_SECRET as string);
+    next();
+  } catch (ex) {
+    return response.status(401).json({ message: "unauthorized" });
+  }
+};
