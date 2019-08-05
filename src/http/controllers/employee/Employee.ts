c60d1e573228a0e77556c71ff2b09eea7f67370f
Bruno <brunotj2015@hotmail.com>
2019-08-04 22:09:47 -0300

    :sparkles: adds Employee registration

--- src/http/controllers/employee/Employee.ts
+++ src/http/controllers/employee/Employee.ts
@@ -1,8 +1,71 @@
 import { Request, Response } from "express";
-import { Employee } from "../../../database/models/Employee";
 
-class EmployeeController {
-  public search = async (
+import { Employee, IEmployee } from "../../../database/models/Employee";
+
+import * as yup from "yup";
+import {
+  IFormattedYupError,
+  format_yup_error
+} from "../../../utils/FormatYupError";
+
+import { ICompany, Company } from "../../../database/models/Company";
+
+import { Maybe } from "../../../types/Maybe";
+
+const RegisterSchema = yup.object().shape({
+  first_name: yup
+    .string()
+    .min(5, "First name must be at least 5 characters long")
+    .max(255, "First name can't be longer than 255 characters")
+    .required(),
+  last_name: yup
+    .string()
+    .min(5, "Last name must be at least 5 characters long")
+    .max(255, "Last name can't be longer than 255 characters")
+    .required(),
+  email: yup
+    .string()
+    .min(5, "Email must be at least 5 characters long")
+    .max(255, "Email can't be longer than 255 characters")
+    .email("Email must be a valid email")
+    .required(),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .max(255)
+    .required(),
+  age: yup
+    .number()
+    .min(18, "Employee must be at least 18 years old")
+    .max(150, "Employee must be alive")
+    .required(),
+  salary: yup
+    .number()
+    .min(1, "Employee must have a salary")
+    .max(Infinity)
+    .required(),
+  occupation: yup
+    .string()
+    .min(3, "Employee must have an occupation")
+    .max(255, "Ocuppation can't be longer than 255 characters")
+    .required(),
+  employer: yup.string().required()
+});
+
+const validate = async (
+  employee: IEmployee
+): Promise<Array<IFormattedYupError>> => {
+  try {
+    console.log(employee);
+    await RegisterSchema.validate(employee, { abortEarly: false });
+    return [];
+  } catch (ex) {
+    return format_yup_error(ex);
+  }
+};
+
+export class EmployeeController {
+  public static search = async (
     request: Request,
     response: Response
   ): Promise<Response> => {
@@ -24,12 +87,12 @@ class EmployeeController {
     }
   };
 
-  public index = async (
+  public static index = async (
     request: Request,
     response: Response
   ): Promise<Response> => response.json(await Employee.find({}));
 
-  public get = async (
+  public static get = async (
     request: Request,
     response: Response
   ): Promise<Response> => {
@@ -42,33 +105,58 @@ class EmployeeController {
     }
   };
 
-  public signup = async (
+  public static register = async (
+    request: Request,
+    response: Response
+  ): Promise<Response> => {
+    try {
+      console.log("body", request.body);
+
+      const errors: Array<IFormattedYupError> = await validate(request.body);
+
+      if (errors.length > 0) return response.status(422).json(errors);
+
+      const employer: Maybe<ICompany> = await Company.findOne({
+        name: request.body.employer
+      });
+
+      if (!employer)
+        return response
+          .status(422)
+          .json({ message: "employer not found, make sure it's registered" });
+
+      request.body.employer = employer._id;
+
+      const employee: IEmployee = await Employee.create(request.body);
+      delete employee.password;
+
+      return response.status(201).json(await Employee.create(employee));
+    } catch (ex) {
+      console.error(ex);
+      return response
+        .status(422)
+        .json({ message: "failed to register employee" });
+    }
+  };
+
+  public static login = async (
     request: Request,
     response: Response
   ): Promise<Response> => {
     return response.send();
   };
 
-  public login = async (
+  public static patch = async (
     request: Request,
     response: Response
   ): Promise<Response> => {
     return response.send();
   };
 
-  public patch = async (
-    request: Request,
-    response: Response
-  ): Promise<Response> => {
-    return response.send();
-  };
-
-  public delete = async (
+  public static delete = async (
     request: Request,
     response: Response
   ): Promise<Response> => {
     return response.send();
   };
 }
-
-export default new EmployeeController();
