a8394eda129aa0e9f0fa3a3d064e527650dd8b73
Bruno <brunotj2015@hotmail.com>
2019-08-05 01:39:27 -0300

    :bug: removes pre hook that caused memory leak

--- src/database/models/Register.ts
+++ src/database/models/Register.ts
@@ -1,7 +1,5 @@
 import Mongoose from "../MongoDB";
 
-import { NextFunction } from "express";
-
 const RegisterSchema = new Mongoose.Schema(
   {
     employee: {
@@ -25,15 +23,6 @@ const RegisterSchema = new Mongoose.Schema(
     timestamps: true
   }
 );
-
-async function populate(this: any, next: NextFunction): Promise<void> {
-  await this.populate("employee");
-  next();
-}
-
-RegisterSchema.pre("find", populate);
-RegisterSchema.pre("findOne", populate);
-RegisterSchema.pre("findById", populate);
 
 export interface IRegister extends Mongoose.Document {
   employee: Mongoose.Types.ObjectId;
